315f2014c3b7b452c228a5461f60ce2214b6fafe
Your Name <niklas1sandberg@gmail.com>
2025-02-20 12:23:36 +0100

    synth_wavetable_256 - added AudioWavetable256Modulated

--- synth_wavetable_256.cpp
+++ synth_wavetable_256.cpp
@@ -87,7 +87,6 @@
           } else {
             val -= multiply_32x32_rshift32( positive,modMagnitude);
           }
-          debugFlag = val;
         }
         uint8_t wtIndex0 = val >> 24;
         uint8_t wtIndex2 = wtIndex0 + 1; //wrap
@@ -132,3 +131,153 @@
      release(block);
  }
  
+ void AudioWavetable256Modulated::update(void)
+{
+  audio_block_t *block, *freqScanWavetable, *ampScanWavetable, *moddata;
+	int16_t *bp, *mbp=0, *end;
+  int32_t val11, val12, val21, val22;
+	int16_t magnitude15;
+	uint32_t i, ph, index, index2, scale, priorphase;
+	const uint32_t inc = phase_increment;
+
+	moddata = receiveReadOnly(1);
+
+	// Pre-compute the phase angle for every output sample of this update
+	ph = phase_accumulator;
+	priorphase = phasedata[AUDIO_BLOCK_SAMPLES-1];
+	if (moddata && modulation_type == 0) {
+    debugFlag = 1;
+		// Frequency Modulation
+		bp = moddata->data;
+		for (i=0; i < AUDIO_BLOCK_SAMPLES; i++) {
+			int32_t n = (*bp++) * modulation_factor; // n is # of octaves to mod
+			int32_t ipart = n >> 27; // 4 integer bits
+			n &= 0x7FFFFFF;          // 27 fractional bits
+			#ifdef IMPROVE_EXPONENTIAL_ACCURACY
+			// exp2 polynomial suggested by Stefan Stenzel on "music-dsp"
+			// mail list, Wed, 3 Sep 2014 10:08:55 +0200
+			int32_t x = n << 3;
+			n = multiply_accumulate_32x32_rshift32_rounded(536870912, x, 1494202713);
+			int32_t sq = multiply_32x32_rshift32_rounded(x, x);
+			n = multiply_accumulate_32x32_rshift32_rounded(n, sq, 1934101615);
+			n = n + (multiply_32x32_rshift32_rounded(sq,
+				multiply_32x32_rshift32_rounded(x, 1358044250)) << 1);
+			n = n << 1;
+			#else
+			// exp2 algorithm by Laurent de Soras
+			// https://www.musicdsp.org/en/latest/Other/106-fast-exp2-approximation.html
+			n = (n + 134217728) << 3;
+
+			n = multiply_32x32_rshift32_rounded(n, n);
+			n = multiply_32x32_rshift32_rounded(n, 715827883) << 3;
+			n = n + 715827882;
+			#endif
+			uint32_t scale = n >> (14 - ipart);
+			uint64_t phstep = (uint64_t)inc * scale;
+			uint32_t phstep_msw = phstep >> 32;
+			if (phstep_msw < 0x7FFE) {
+				ph += phstep >> 16;
+			} else {
+				ph += 0x7FFE0000;
+			}
+			phasedata[i] = ph;
+		}
+		release(moddata);
+	} else if (moddata) {
+		// Phase Modulation
+		bp = moddata->data;
+		for (i=0; i < AUDIO_BLOCK_SAMPLES; i++) {
+			// more than +/- 180 deg shift by 32 bit overflow of "n"
+		        uint32_t n = ((uint32_t)(*bp++)) * modulation_factor;
+			phasedata[i] = ph + n;
+			ph += inc;
+		}
+		release(moddata);
+	} else {
+		// No Modulation Input
+		for (i=0; i < AUDIO_BLOCK_SAMPLES; i++) {
+			phasedata[i] = ph;
+			ph += inc;
+		}
+	}
+	phase_accumulator = ph;
+
+  if(wave_tables==NULL) {
+  return;
+  }
+
+  if (magnitude == 0) {
+      return;
+  }
+  
+  block = allocate();
+  
+  if (!block) {
+      return;
+  }
+  
+freqScanWavetable = receiveReadOnly(0);
+
+bp = block->data;
+
+if(freqScanWavetable) {
+  mbp = freqScanWavetable->data;
+}
+
+// len = 256
+for (i=0; i < AUDIO_BLOCK_SAMPLES; i++) {
+    ph = phasedata[i];
+    index = ph >> 24;
+    index2 = index + 1;
+    //if (index2 >= 256) index2 = 0; //wrap
+    
+    
+    uint32_t val = scanw;
+    if(mbp) {
+      int16_t modVal = *mbp++;
+      int32_t positive = uint32_t(32768+int32_t(modVal)) << 16;
+      if(modVal>=0) {
+        val += multiply_32x32_rshift32( positive,modMagnitude);
+      } else {
+        val -= multiply_32x32_rshift32( positive,modMagnitude);
+      }
+    }
+    uint8_t wtIndex0 = val >> 24;
+    uint8_t wtIndex2 = wtIndex0 + 1; //wrap
+    //if (index2 >= 256) index2 = 0; //wrap
+    uint32_t interpolate = val & 0xFFFFFF;
+
+    int16_t *arbdata1 = wave_tables[wtIndex0];
+    int16_t *arbdata2 = wave_tables[wtIndex2];
+
+    val11 = *(arbdata1 + index);
+    val12 = *(arbdata1 + index2);
+    val21 = *(arbdata2 + index); //TODO
+    val22 = *(arbdata2 + index2); //TODO
+    scale = (ph >> 8) & 0xFFFF;
+    val12 *= scale;
+    val11 *= 0x10000 - scale;
+    val22 *= scale;
+    val21 *= 0x10000 - scale;
+    int32_t out1 = multiply_32x32_rshift32(val11 + val12, magnitude);
+    int32_t out2 = multiply_32x32_rshift32(val21 + val22, magnitude);
+
+    int32_t out_scan_1 = multiply_32x32_rshift32( (0x1000000 - interpolate)*0x10,out1*0x10);
+    int32_t out_scan_2 = multiply_32x32_rshift32(interpolate*0x10,out2*0x10);
+    *bp++ = out_scan_2 + out_scan_1;
+    //*bp++ = out1;
+}
+
+  if (tone_offset) {
+      bp = block->data;
+      end = bp + AUDIO_BLOCK_SAMPLES;
+      do {
+          val11 = *bp;
+          *bp++ = signed_saturate_rshift(val11 + tone_offset, 16, 0);
+      } while (bp < end);
+  }
+
+  if(freqScanWavetable) release(freqScanWavetable);
+  transmit(block, 0);
+  release(block);
+}
--- synth_wavetable_256.h
+++ synth_wavetable_256.h
@@ -70,14 +70,6 @@
 		 }
 		 magnitude = n * 65536.0f;
 	 }
-	 void modAmplitude(float n) {
-		 if (n < 0) {
-			 n = 0;
-		 } else if (n > 1.0f) {
-			 n = 1.0;
-		 }
-		 modMagnitude = n * 2147483647.0f;
-	 }
 	 void offset(float n) {
 		 if (n < -1.0f) {
 			 n = -1.0f;
@@ -95,6 +87,15 @@
 		 phase_offset = 0;
 		 begin (t_type);
 	 }
+
+	 void modAmplitude(float n) {
+		 if (n < 0) {
+			 n = 0;
+		 } else if (n > 1.0f) {
+			 n = 1.0;
+		 }
+		 modMagnitude = n * 2147483647.0f;
+	 }
 	 void arbitraryWaveforms(int16_t *data[256], const int32_t num) {
 		if(num != 256) return;
 		wave_tables = data;
@@ -106,9 +107,8 @@
 		 }
 		 scanw = s * 4294967295.f;
 	 }
+	 
 	 virtual void update(void);
- 
-	 int32_t debugFlag;
 
  private:
  	audio_block_t *inputQueueArray[1];
@@ -122,5 +122,108 @@
 	 int16_t  tone_offset;
  };
  
+ class AudioWavetable256Modulated : public AudioStream
+ {
+ public:
+	 AudioWavetable256Modulated(void) : AudioStream(2, inputQueueArray),
+		 phase_accumulator(0), phase_increment(0), 
+		 //phase_offset(0), //TODO: added, needed I think but not in org impl. 
+		 modulation_factor(32768),
+		 magnitude(0), modMagnitude(0), wave_tables(NULL), tone_offset(0),
+		 scanw(0), modulation_type(0) {
+	 }
+ 
+	 void frequency(float freq) {
+		 if (freq < 0.0f) {
+			 freq = 0.0;
+		 } else if (freq > AUDIO_SAMPLE_RATE_EXACT / 2.0f) {
+			 freq = AUDIO_SAMPLE_RATE_EXACT / 2.0f;
+		 }
+		 phase_increment = freq * (4294967296.0f / AUDIO_SAMPLE_RATE_EXACT);
+		 if (phase_increment > 0x7FFE0000u) phase_increment = 0x7FFE0000;
+	 }
+	 void amplitude(float n) {	// 0 to 1.0
+		 if (n < 0) {
+			 n = 0;
+		 } else if (n > 1.0f) {
+			 n = 1.0f;
+		 }
+		 magnitude = n * 65536.0f;
+	 }
+	 void offset(float n) {
+		 if (n < -1.0f) {
+			 n = -1.0f;
+		 } else if (n > 1.0f) {
+			 n = 1.0f;
+		 }
+		 tone_offset = n * 32767.0f;
+	 }
+	 void begin(short t_type) {
+		 tone_type = t_type;
+	 }
+	 void begin(float t_amp, float t_freq, short t_type) {
+		 amplitude(t_amp);
+		 frequency(t_freq);
+		 begin (t_type) ;
+	 }
+	 void frequencyModulation(float octaves) {
+		 if (octaves > 12.0f) {
+			 octaves = 12.0f;
+		 } else if (octaves < 0.1f) {
+			 octaves = 0.1f;
+		 }
+		 modulation_factor = octaves * 4096.0f;
+		 modulation_type = 0;
+	 }
+	 void phaseModulation(float degrees) {
+		 if (degrees > 9000.0f) {
+			 degrees = 9000.0f;
+		 } else if (degrees < 30.0f) {
+			 degrees = 30.0f;
+		 }
+		 modulation_factor = degrees * (float)(65536.0 / 180.0);
+		 modulation_type = 1;
+	 }
+
+	 void modAmplitude(float n) {
+		 if (n < 0) {
+			 n = 0;
+		 } else if (n > 1.0f) {
+			 n = 1.0;
+		 }
+		 modMagnitude = n * 2147483647.0f;
+	 }
+	 void arbitraryWaveforms(int16_t *data[256], const int32_t num) {
+		if(num != 256) return;
+		wave_tables = data;
+	 }
+	 
+	 void waveSelect(float s) {
+		 if(s > 1.f) {
+			 s = 1.f;
+		 }
+		 scanw = s * 4294967295.f;
+	 }
+	 
+	 virtual void update(void);
+
+	 int32_t debugFlag;
+ 
+ private:
+	 audio_block_t *inputQueueArray[2];
+	 uint32_t phase_accumulator;
+	 uint32_t phase_increment;
+	 uint32_t phase_offset;
+	 uint32_t modulation_factor;
+	 int32_t  magnitude;
+	 int32_t  modMagnitude;
+	 const int16_t **wave_tables;
+	 uint32_t phasedata[AUDIO_BLOCK_SAMPLES];
+	 uint32_t scanw;
+	 int16_t  tone_offset;
+	 uint8_t  tone_type;
+	 uint8_t  modulation_type;
+ };
+
  #endif
  
